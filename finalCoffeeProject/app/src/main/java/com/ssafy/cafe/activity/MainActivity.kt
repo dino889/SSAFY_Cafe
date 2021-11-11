c2a00c0a03e091321c04df933d759034217075bc
leeboyeon <qhdus101@gmail.com>
2021-11-11 23:47:48 +0900

    login fragment, join fragment modify

--- finalCoffeeProject/app/src/main/java/com/ssafy/cafe/activity/MainActivity.kt
+++ finalCoffeeProject/app/src/main/java/com/ssafy/cafe/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.ssafy.cafe
+package com.ssafy.cafe.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ssafy.cafe.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
