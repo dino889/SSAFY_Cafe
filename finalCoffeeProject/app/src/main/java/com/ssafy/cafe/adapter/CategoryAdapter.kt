ffa014773eef9d4405db7c5bf0497efcda50a607
LeeBoyeon <61018094+leeboyeon@users.noreply.github.com>
2021-11-14 23:13:28 +0900

    commit 11-14

---
+++ finalCoffeeProject/app/src/main/java/com/ssafy/cafe/adapter/CategoryAdapter.kt
@@ -0,0 +1,44 @@
+package com.ssafy.cafe.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.ssafy.cafe.R
+import com.ssafy.cafe.dto.Category
+
+class CategoryAdapter(var categoryList:List<Category>) : RecyclerView.Adapter<CategoryAdapter.CategoryHolder>() {
+
+    inner class CategoryHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val category = itemView.findViewById<TextView>(R.id.tv_categoryName)
+        fun bindInfo(categorys: Category){
+            category.text = categorys.category
+            itemView.setOnClickListener {
+                itemClickListener.onClick(it, layoutPosition, categoryList[layoutPosition].id)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_menu_category_list_item,parent,false)
+        return CategoryHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CategoryHolder, position: Int) {
+        holder.apply {
+            bindInfo(categoryList[position])
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return categoryList.size
+    }
+    interface ItemClickListener {
+        fun onClick(view: View, position: Int, categoryId: Int)
+    }
+    private lateinit var itemClickListener: ItemClickListener
+    fun setItemClickListener(itemClickListener: ItemClickListener){
+        this.itemClickListener = itemClickListener
+    }
+}
--- finalCoffeeProject/app/src/main/java/com/ssafy/cafe/fragment/AllMenuFragment.kt
+++ finalCoffeeProject/app/src/main/java/com/ssafy/cafe/fragment/AllMenuFragment.kt
@@ -6,12 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.ssafy.cafe.R
+import com.ssafy.cafe.adapter.CategoryAdapter
 import com.ssafy.cafe.databinding.FragmentAllMenuBinding
+import com.ssafy.cafe.dto.Category
 
 class AllMenuFragment : Fragment() {
-
+    private lateinit var categoryList:List<Category>
     private lateinit var binding: FragmentAllMenuBinding
+    private lateinit var categoryAdapter:CategoryAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,9 +24,24 @@ class AllMenuFragment : Fragment() {
         binding = FragmentAllMenuBinding.inflate(layoutInflater,container,false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initAdapter()
+    }
     fun initAdapter(){
+        categoryList = arrayListOf(
+            Category(1,"coffee"),
+            Category(2,"blendded"),
+            Category(3,"ade"),
+            Category(4,"tea"),
+            Category(5,"dessert"))
+
+        categoryAdapter = CategoryAdapter(categoryList)
         binding.rvCategoryMenu.apply{
             layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+            adapter = categoryAdapter
+            adapter!!.stateRestorationPolicy = RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
         }
     }
     companion
