555e82844c02e442631405f1c1499aa910199436
LeeBoyeon <61018094+leeboyeon@users.noreply.github.com>
2021-11-16 22:41:10 +0900

    commiting

--- finalCoffeeProject/app/src/main/java/com/ssafy/cafe/fragment/MyPageFragment.kt
+++ finalCoffeeProject/app/src/main/java/com/ssafy/cafe/fragment/MyPageFragment.kt
@@ -1,18 +1,23 @@
 package com.ssafy.cafe.fragment
 
 import android.os.Bundle
+import android.util.JsonReader
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import com.ssafy.cafe.R
 import com.ssafy.cafe.config.ApplicationClass
 import com.ssafy.cafe.databinding.FragmentMyPageBinding
+import com.ssafy.cafe.dto.Order
 import com.ssafy.cafe.dto.User
 import com.ssafy.cafe.service.UserService
 import com.ssafy.cafe.util.RetrofitCallback
+import org.json.JSONObject
+import java.io.StringReader
 
 private const val TAG = "MyPageFragment"
 class MyPageFragment : Fragment() {
@@ -44,6 +49,12 @@ class MyPageFragment : Fragment() {
                 //val grade = responseData!!["grade"]
 
                 val user = Gson().fromJson(responseData["user"].toString(), User::class.java)
+                //json : responseData["order"]
+
+                val orderList:List<Order> = Gson().fromJson(responseData["order"].toString(), Array<Order>::class.java).toList()
+                for(i in orderList){
+                    Log.d(TAG, "onSuccess: ${i.orderTable}")
+                }
                 var pay = user.money
                 binding.tvMyPayMoney.text = "$pay 원"
                 ApplicationClass.sharedPreferencesUtil.addUserPay(pay)
