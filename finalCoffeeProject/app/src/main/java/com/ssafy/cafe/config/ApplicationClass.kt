9adeb241b8160016ee0066871e1d712554376944
dino889 <63338678+dino889@users.noreply.github.com>
2021-11-17 00:35:44 +0900

    fix) init - menuDetailFragment

--- finalCoffeeProject/app/src/main/java/com/ssafy/cafe/config/ApplicationClass.kt
+++ finalCoffeeProject/app/src/main/java/com/ssafy/cafe/config/ApplicationClass.kt
@@ -13,11 +13,11 @@ import java.util.concurrent.TimeUnit
 class ApplicationClass : Application() {
     companion object{
         //boyeon serverurl
-        const val SERVER_URL = "http://1.251.96.80:8888/"
+//        const val SERVER_URL = "http://1.251.96.80:8888/"
 
         //jiwoo serverurl
 //        const val SERVER_URL = "http://172.30.1.36:8888/"
-//        const val SERVER_URL = "http://172.30.1.14:8888/"   // desktop
+        const val SERVER_URL = "http://172.30.1.14:8888/"   // desktop
 
         const val MENU_IMGS_URL = "${SERVER_URL}imgs/menu/"
 //        const val IMGS_URL = "${SERVER_URL}imgs/"
--- finalCoffeeProject/app/src/main/java/com/ssafy/cafe/fragment/MenuDetailFragment.kt
+++ finalCoffeeProject/app/src/main/java/com/ssafy/cafe/fragment/MenuDetailFragment.kt
@@ -1,25 +1,40 @@
 package com.ssafy.cafe.fragment
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.ssafy.cafe.R
 import com.ssafy.cafe.activity.MainActivity
+import com.ssafy.cafe.config.ApplicationClass
+import com.ssafy.cafe.databinding.FragmentMenuDetailBinding
+import com.ssafy.cafe.response.MenuDetailWithCommentResponse
+import com.ssafy.cafe.service.ProductService
+import com.ssafy.cafe.util.CommonUtils
+import com.ssafy.cafe.util.RetrofitCallback
+import retrofit2.Retrofit
 
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
+private const val TAG = "MenuDetailFragment_싸피"
 class MenuDetailFragment : Fragment() {
     private lateinit var mainActivity : MainActivity
-    
+    private lateinit var binding : FragmentMenuDetailBinding
+
     private var productId = -1
 
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mainActivity.hideBottomNav(true)
         arguments?.let {
             productId = it.getInt("productId", -1)
         }
@@ -29,26 +44,87 @@ class MenuDetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu_detail, container, false)
+        binding = FragmentMenuDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initData()
+        countProduct()
+    }
+
+    private fun initData() {
+        ProductService().getProductWithComments(productId, ProductWithCommentInsertCallback())
+
+    }
+
+    private fun initScreen(menu : MenuDetailWithCommentResponse) {
+        Glide.with(this).load("${ApplicationClass.MENU_IMGS_URL}${menu.productImg}").into(binding.ivCafeMenuImg)
+        binding.tvCafeMenuName.text = menu.productName
+        binding.tvCafeMenuPrice.text = CommonUtils.makeComma(menu.productPrice)
+
+        binding.hoticeIce.isChecked = true
+    }
+
+    private fun countProduct() {
+        var tmp = binding.tvCafeMenuCnt.text.toString()
+        var menuCnt = tmp[0].toString().toInt()
+//        Log.d(TAG, "countProduct: ${tmp[0]} ****** ${tmp[1]}")
+        binding.ibtnAddCount.setOnClickListener {
+            menuCnt++
+            binding.tvCafeMenuCnt.text = "${menuCnt}개"
+        }
+
+        binding.ibtnMinusCount.setOnClickListener {
+            if(menuCnt <= 0) {
+                Toast.makeText(requireContext(), "수량을 입력해주세요.", Toast.LENGTH_SHORT).show()
+                binding.tvCafeMenuCnt.text = "0개"
+            } else {
+                menuCnt--
+                binding.tvCafeMenuCnt.text = "${menuCnt}개"
+            }
+        }
+    }
+
+
+    inner class ProductWithCommentInsertCallback : RetrofitCallback<List<MenuDetailWithCommentResponse>> {
+        override fun onError(t: Throwable) {
+            Log.d(TAG, t.message ?: "물품 정보를 받아오는 중 통신오류")
+        }
+
+        override fun onSuccess(code: Int, responseData: List<MenuDetailWithCommentResponse>) {
+            // comment가 없을 경우 -> 들어온 response가 1개이고 해당 userId가 null일 경우 빈 배열 adapter에 연결
+//            commentAdapter = if(responseData.size == 1 && responseData[0].userId == null) {
+//                CommentAdapter(mutableListOf(), this@MenuDetailFragment::initData)
+//            } else {
+//                CommentAdapter(responseData, this@MenuDetailFragment::initData)
+//            }
+//            liveData.value = responseData
+
+
+            initScreen(responseData[0])
+        }
+
+        override fun onFailure(code: Int) {
+            Log.d(TAG, "onFailure: Error Code $code")
+        }
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mainActivity.hideBottomNav(false)
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MenuDetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(key: String, value : Int) =
             MenuDetailFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putInt(key, value)
                 }
             }
     }
