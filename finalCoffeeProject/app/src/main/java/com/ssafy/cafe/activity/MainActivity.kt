4d1aed1c12ec073338cf8f03866ed8eb4ccd9978
dino889 <63338678+dino889@users.noreply.github.com>
2021-11-19 00:55:44 +0900

    clear) normal-21_beacon

--- finalCoffeeProject/app/src/main/java/com/ssafy/cafe/activity/MainActivity.kt
+++ finalCoffeeProject/app/src/main/java/com/ssafy/cafe/activity/MainActivity.kt
@@ -1,20 +1,89 @@
 package com.ssafy.cafe.activity
 
+import android.Manifest
+import android.app.Dialog
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.nfc.NfcAdapter
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.RemoteException
+import android.util.Log
 import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
 import com.ssafy.cafe.R
 import com.ssafy.cafe.config.ApplicationClass
 import com.ssafy.cafe.databinding.ActivityMainBinding
 import com.ssafy.cafe.fragment.*
+import com.ssafy.cafe.response.LatestOrderResponse
+import com.ssafy.cafe.service.OrderService
+import org.altbeacon.beacon.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BeaconConsumer {
+    private val TAG = "MainActivity_싸피"
     private lateinit var binding: ActivityMainBinding
+
+    // ---------------------------------------------------------------------------------------------
+    // Beacon 관련 변수
+    private lateinit var beaconManager: BeaconManager
+    private val BEACON_UUID = "fda50693-a4e2-4fb1-afcf-c6eb07647825"
+    private val BEACON_MAJOR = "10004"
+    private val BEACON_MINOR = "54480"
+
+    private val STORE_DISTANCE = 1
+    private val region = Region("altbeacon",
+        Identifier.parse(BEACON_UUID),
+        Identifier.parse(BEACON_MAJOR),
+        Identifier.parse(BEACON_MINOR))
+//    private val region = Region("altbeacon", null, null, null)
+
+    private lateinit var bluetoothManager: BluetoothManager
+    private var bluetoothAdapter: BluetoothAdapter? = null
+    private var needBLERequest = true
+
+    private val PERMISSIONS_CODE = 100
+    private var isDialogCalled = false
+
+    // 모든 퍼미션 관련 배열
+    private val requiredPermissions = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+    )
+
+    // dialog
+    private lateinit var dialog : Dialog
+
+    // lastorder
+    private var lastOrder : LatestOrderResponse? = null
+    private var isLastOrderLoaded = false
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        getLastOrder()
+
+        dialog = Dialog(this)
+
+        setBeacon()
+
+        checkPermissions()
 
         supportFragmentManager.beginTransaction()
             .replace(R.id.frame_layout_main, HomeFragment())
@@ -150,5 +219,213 @@ class MainActivity : AppCompatActivity() {
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
         startActivity(intent)
+    }
+
+    // ---------------------------------------------------------------------------------------------
+    // ---------------------------------------------------------------------------------------------
+    // Beacon 관련 함수
+    // 비콘과 사용자 거리가 1M 이내이면 dialog 띄우기 (매장 내에 비콘이 설치되어 있다 가정)
+
+    // beacon manager, adapter 연결
+    private fun setBeacon(){
+        beaconManager = BeaconManager.getInstanceForApplication(this)
+        beaconManager.beaconParsers.add(BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"))
+        bluetoothManager = getSystemService(BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothAdapter = bluetoothManager.adapter
+    }
+
+    // 위치 권한 체크
+    private fun checkPermissions(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(
+                this,
+                requiredPermissions,
+                PERMISSIONS_CODE
+            )
+        }
+    }
+
+    // 블루투스 켰는지 확인
+    private fun isEnableBLEService(): Boolean{
+        if(!bluetoothAdapter!!.isEnabled){
+            Log.d(TAG, "isEnableBLEService: false ")
+            return false
+        }
+        Log.d(TAG, "isEnableBLEService: true")
+        return true
+    }
+
+    // Beacon Scan 시작
+    private fun startScan() {
+        // 블루투스 Enable 확인
+        if(!isEnableBLEService()){
+            requestEnableBLE()
+            Log.d(TAG, "startScan: 블루투스가 켜지지 않았습니다.")
+            return
+        }
+
+        // 위치 정보 권한 허용 및 GPS Enable 여부 확인
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(
+                this,
+                requiredPermissions,
+                PERMISSIONS_CODE
+            )
+        }
+        Log.d(TAG, "startScan: beacon Scan start")
+
+        // Beacon Service bind
+        beaconManager.bind(this)
+    }
+
+    // 블루투스 ON/OFF 여부 확인 및 키도록 하는 함수
+    private fun requestEnableBLE(){
+        val callBLEEnableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+        requestBLEActivity.launch(callBLEEnableIntent)
+        Log.d(TAG, "requestEnableBLE: ")
+    }
+
+    private val requestBLEActivity: ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){
+        // 사용자의 블루투스 사용이 가능한지 확인
+        if (isEnableBLEService()) {
+            needBLERequest = false
+            startScan()
+        }
+    }
+
+    // 위치 정보 권한 요청 결과 콜백 함수
+    // ActivityCompat.requestPermissions 실행 이후 실행
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSIONS_CODE -> {
+                if(grantResults.isNotEmpty()) {
+                    for((i, permission) in permissions.withIndex()) {
+                        if(grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                            //권한 획득 실패
+                            Log.i(TAG, "$permission 권한 획득에 실패하였습니다.")
+                            finish()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onBeaconServiceConnect() {
+        beaconManager.addMonitorNotifier(object : MonitorNotifier {
+            override fun didEnterRegion(region: Region?) {
+                try {
+                    Log.d(TAG, "비콘을 발견하였습니다.------------${region.toString()}")
+                    beaconManager.startRangingBeaconsInRegion(region!!)
+                } catch (e: RemoteException) {
+                    e.printStackTrace()
+                }
+
+            }
+            override fun didExitRegion(region: Region?) {
+                try {
+                    Log.d(TAG, "비콘을 찾을 수 없습니다.")
+                    beaconManager.stopRangingBeaconsInRegion(region!!)
+                } catch (e: RemoteException) {
+                    e.printStackTrace()
+                }
+            }
+            override fun didDetermineStateForRegion(i: Int, region: Region?) {}
+        })
+        try {
+            beaconManager.startMonitoringBeaconsInRegion(region)
+            Log.d(TAG, "onBeaconServiceConnect: ${region.id1}")
+
+        } catch (e: RemoteException){
+            e.printStackTrace()
+        }
+        beaconManager.addRangeNotifier { beacons, region ->
+            for (beacon in beacons) {
+                // Major, Minor로 Beacon 구별, 1미터 이내에 들어오면 메세지 출력
+
+                if(isYourBeacon(beacon)){
+                    if(isDialogCalled == false && isLastOrderLoaded == true) {
+//                        showDialog()
+                        runOnUiThread{
+                            showDialog()
+                        }
+                    }
+                }
+            }
+
+            if(beacons.isEmpty()){
+                Log.d(TAG, "onBeaconServiceConnect: isEMpty")
+            }
+        }
+    }
+
+    // 찾고자 하는 Beacon이 맞는지, 정해둔 거리 내부인지 확인
+    private fun isYourBeacon(beacon: Beacon): Boolean {
+
+        Log.d(TAG, "isYourBeacon: ")
+        return (beacon.id2.toString() == BEACON_MAJOR &&
+                beacon.id3.toString() == BEACON_MINOR &&
+                beacon.distance <= STORE_DISTANCE   // 1M안 비콘들을 모두 찾으시오.
+                )
+//        return (beacon.distance <= STORE_DISTANCE)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startScan()
+    }
+
+    // beacon 스캔 중지
+    override fun onPause() {
+        beaconManager.stopMonitoringBeaconsInRegion(region)
+        beaconManager.stopRangingBeaconsInRegion(region)
+        beaconManager.unbind(this)
+        super.onPause()
+    }
+
+    private fun showDialog() {
+        isDialogCalled = true
+
+        dialog.setContentView(R.layout.dialog_notice_info)
+
+        var menuImg = dialog.findViewById<ImageView>(R.id.dialog_menuImg)
+        var menuName = dialog.findViewById<TextView>(R.id.dialog_menuName)
+        var menuPrice = dialog.findViewById<TextView>(R.id.dialog_menuPrice)
+        var confirm = dialog.findViewById<TextView>(R.id.dialog_confirm)
+
+//        dialog.findViewById<LinearLayout>(R.id.dialog_lastOrder).visibility = View.VISIBLE
+
+        if(lastOrder != null) {
+            dialog.findViewById<LinearLayout>(R.id.dialog_lastOrder).visibility = View.VISIBLE
+            Glide.with(this@MainActivity).load("${ApplicationClass.MENU_IMGS_URL}${lastOrder!!.img}").into(menuImg)
+            menuName.text = lastOrder!!.productName
+            menuPrice.text = "${lastOrder!!.orderCnt}잔"
+        } else {
+            dialog.findViewById<LinearLayout>(R.id.dialog_lastOrder).visibility = View.GONE
+            dialog.findViewById<TextView>(R.id.dialog_tmp).visibility = View.VISIBLE
+        }
+
+        confirm.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.show()
+    }
+
+
+    fun getLastOrder() {
+        val live = OrderService().getLastMonthOrder(ApplicationClass.sharedPreferencesUtil.getUser().id)
+        Log.d(TAG, "getLastOrder: ${live.value.toString()}")
+        live.observe(this) {
+            if(it != null && it.isNotEmpty()) lastOrder = it[0] // 가장 최근 주문 내역 1건
+            Log.d(TAG, "getLastOrder: ${it[0]}")
+            isLastOrderLoaded = true
+        }
     }
 }
