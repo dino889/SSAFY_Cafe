41b8215f123ada6c98b7b36a7513fa38d7b7dc27
LeeBoyeon <61018094+leeboyeon@users.noreply.github.com>
2021-11-20 00:18:06 +0900

    error-11 commit
    shopping Adapter Edit Option

--- finalCoffeeProject/app/src/main/java/com/ssafy/cafe/adapter/ShoppingCartAdapter.kt
+++ finalCoffeeProject/app/src/main/java/com/ssafy/cafe/adapter/ShoppingCartAdapter.kt
@@ -1,5 +1,6 @@
 package com.ssafy.cafe.adapter
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,6 +12,7 @@ import com.ssafy.cafe.R
 import com.ssafy.cafe.dto.ShoppingCart
 import com.ssafy.cafe.util.CommonUtils
 
+private const val TAG = "ShoppingCartAdapter"
 class ShoppingCartAdapter : RecyclerView.Adapter<ShoppingCartAdapter.ShoppingListHolder>(){
 
     var list = mutableListOf<ShoppingCart>()
@@ -28,6 +30,33 @@ class ShoppingCartAdapter : RecyclerView.Adapter<ShoppingCartAdapter.ShoppingLis
             itemView.findViewById<TextView>(R.id.tv_cafeMenuCnt).text = "${data.menuCnt}개"
             itemView.findViewById<TextView>(R.id.tv_menuPrice).text = "${CommonUtils.makeComma(data.menuPrice)}"
             itemView.findViewById<TextView>(R.id.tv_menuTotalPrice).text = "${CommonUtils.makeComma(data.totalPrice)}"
+
+            Log.d(TAG, "bindInfo: ${data.shot} | ${data.syrup} ")
+
+            val option = if(data.syrup == null || data.syrup.toString().equals("null")){
+                    Log.d(TAG, "bindInfo: noSyrup")
+                if(data.shot == 0){
+                    "없음"
+                }else{
+                    "샷: ${data.shot}개추가"
+                }
+            }else if(data.syrup.equals("설탕")){
+                Log.d(TAG, "bindInfo: this is sugar")
+
+                if(data.shot == 0){
+                    "시럽: ${data.syrup}(+0원) | 샷: 없음"
+                }else{
+                    "시럽: ${data.syrup}(+0원) | 샷: ${data.shot}개추가"
+                }
+            }else{
+                ""
+                if(data.shot == 0){
+                    "시럽: ${data.syrup}(+500원) | 없음"
+                }else{
+                   "시럽: ${data.syrup}(+500원) | 샷: ${data.shot}개추가"
+                }
+            }
+            itemView.findViewById<TextView>(R.id.tv_menuOption).text = option
         }
 
     }
