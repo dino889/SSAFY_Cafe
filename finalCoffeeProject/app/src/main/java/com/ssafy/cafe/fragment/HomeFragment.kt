21f86286ad429450a879cd1bb9a8b7eab1c69696
LeeBoyeon <61018094+leeboyeon@users.noreply.github.com>
2021-11-20 18:19:43 +0900

    commit

--- finalCoffeeProject/app/src/main/java/com/ssafy/cafe/fragment/HomeFragment.kt
+++ finalCoffeeProject/app/src/main/java/com/ssafy/cafe/fragment/HomeFragment.kt
@@ -156,7 +156,7 @@ class HomeFragment : Fragment() {
                 for(i in viewModel.productList!!.indices) {
                     val product = viewModel.productList!![i]
                     if(item.productName.equals(product.name)) {
-                        //   viewModel.insertShoppingCartItem(ShoppingCart( product.id, product.img, product.name, item.quantity, item.unitPrice,))
+//                        viewModel.insertShoppingCartItem(ShoppingCart( product.id, product.img, product.name, item.quantity, item.unitPrice,))
                         break
                     }
                 }
