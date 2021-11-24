38660878fec92cce28265194c1f0b0c2ecada209
LeeBoyeon <61018094+leeboyeon@users.noreply.github.com>
2021-11-25 02:38:33 +0900

    loading dialog happy...

--- finalCoffeeProject/app/src/main/java/com/ssafy/cafe/fragment/AllMenuFragment.kt
+++ finalCoffeeProject/app/src/main/java/com/ssafy/cafe/fragment/AllMenuFragment.kt
@@ -3,6 +3,8 @@ package com.ssafy.cafe.fragment
 import android.content.Context
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
+import android.os.Message
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -96,14 +98,21 @@ class AllMenuFragment : BaseFragment<FragmentAllMenuBinding>(FragmentAllMenuBind
 
         categoryAdapter.setItemClickListener(object : CategoryAdapter.ItemClickListener {
             override fun onClick(view: View, position: Int, categoryId: Int) {
+
                 val category = categoryList[position]
-                if(position == 0){
-                    ProductService().getProductList(ProductCallback())
-                    binding.llSearchlayout.isVisible = true
-                }else{
-                    binding.llSearchlayout.isVisible = false
-                    ProductService().getProductWithTypeList(category.category, ProductCallback())
-                }
+
+                Handler().postDelayed({
+                    if(position == 0){
+                        ProductService().getProductList(ProductCallback())
+                        binding.llSearchlayout.isVisible = true
+                    }else{
+                        binding.llSearchlayout.isVisible = false
+                        ProductService().getProductWithTypeList(category.category, ProductCallback())
+                        dismissLoadingDialog()
+                    }
+                },500)
+                showLoadingDialog(requireContext())
+
 
 
             }
